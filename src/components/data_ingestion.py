8b51f332e7483a7cedcac925cf990814dd704ccc
NarnindiMounica <mounica.narnindi12@gmail.com>
2023-08-15 00:19:06 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,11 @@
 import os
 import sys
-
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 from src.loggers import logging
 from src.exception import CustomException
-import pandas as pd 
-
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
@@ -44,4 +44,7 @@ class DataIngestion:
 
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path=obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_path, test_path)
